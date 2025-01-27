d40037241b68d472642fcec6322d85cd41a5ec76
Alfio <tomarchioalfio5@gmail.com>
2025-01-27 15:41:51 +0000

    remove templates and imports after processing

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Xenon: Static HTML Components in 650 Bytes
 
-Xenon is a small library that implements declarative, reusable components into vanilla HTML, with no user-side javascript or build step.
+Xenon is a small library that implements declarative, reusable components into static HTML, with no javascript interaction or build step.
 
-The entire library uses exactly 648 bytes of javascript.
+The entire library uses exactly 627 bytes of javascript.
 
 ## Motivation
 
@@ -31,7 +31,7 @@ Alternatively, for faster loading, you can inline Xenon by placing the following
 ```html
 <script defer>
 // https://github.com/p2js/xenon
-(()=>{let e=e=>{let t=e.getAttribute("_");e.removeAttribute("_"),document.querySelectorAll(t).forEach((t=>{let r=e.innerHTML;for(let l of e.getAttributeNames()){let o=t.getAttribute(l)||e.getAttribute(l);r=r.replaceAll("{"+l+"}",o)}r=r.replaceAll("{$children}",t.innerHTML),t.innerHTML=r,t.querySelectorAll("if").forEach((e=>{e.getAttributeNames().some((e=>t.hasAttribute(e)))?e.replaceWith(...e.childNodes):e.remove()})),t.outerHTML=t.innerHTML}))};document.querySelectorAll("template[_]").forEach(e),document.querySelectorAll("iframe.template-import").forEach((t=>{t.onload=()=>t.contentDocument.querySelectorAll("template[_]").forEach(e)}))})();
+(e=>{let t=e=>e.querySelectorAll("template[_]").forEach((e=>{document.querySelectorAll(e.getAttribute("_")).forEach((t=>{let r=e.innerHTML;for(let l of e.getAttributeNames().filter((e=>"_"!=e))){let o=t.getAttribute(l)||e.getAttribute(l);r=r.replaceAll("{"+l+"}",o)}r=r.replaceAll("{$children}",t.innerHTML),t.innerHTML=r,t.querySelectorAll("if").forEach((e=>{e.getAttributeNames().some((e=>t.hasAttribute(e)))?e.replaceWith(...e.childNodes):e.remove()})),t.outerHTML=t.innerHTML})),e.remove()}));t(document),document.querySelectorAll("iframe.template-import").forEach((e=>{e.onload=r=>{t(e.contentDocument),e.remove()}}))})();
 </script>
 ```
 
--- xenon.js
+++ xenon.js
@@ -1,22 +1,22 @@
-(() => {
-    let processComponentTemplate = template => {
-        let componentName = template.getAttribute("_");
-        template.removeAttribute("_");
-        document.querySelectorAll(componentName).forEach(instance => {
+(_ => {
+    // Process a document's component templates and apply them to the current document
+    let processTemplates = doc => doc.querySelectorAll("template[_]").forEach(template => {
+        // Select all elements in the current document with the name specified in the attribute
+        document.querySelectorAll(template.getAttribute("_")).forEach(instance => {
             // Process the template HTML
             let instanceHTML = template.innerHTML;
-            // Get all the attirbutes in the declaration
-            for (let attributeName of template.getAttributeNames()) {
-                // replace all instances of {<attributeName>} with the value given in the instance,
+            // Get the attributes in the declaration
+            for (let attributeName of template.getAttributeNames().filter(name => name != "_")) {
+                // replace all appearances of {<attributeName>} with the value given in the instance,
                 // otherwise use the component's default value
                 let attributeValue = instance.getAttribute(attributeName) || template.getAttribute(attributeName);
                 instanceHTML = instanceHTML.replaceAll("{" + attributeName + "}", attributeValue);
             }
-            // Replace all instances of {$children} with the instance's HTML
+            // Replace all appearances of {$children} with the instance's HTML
             instanceHTML = instanceHTML.replaceAll("{$children}", instance.innerHTML);
-            // Place it inside the instance for if-block processing
+            // Place the processed HTML inside the instance 
             instance.innerHTML = instanceHTML;
-
+            // Process <if> blocks
             instance.querySelectorAll("if").forEach(block => {
                 // show child nodes if any of the attributes in the block are present
                 if (block.getAttributeNames().some(attribute => instance.hasAttribute(attribute))) {
@@ -25,15 +25,18 @@
                     block.remove();
                 };
             });
-
-            // Replace the instance with its processed template HTML
             instance.outerHTML = instance.innerHTML;
         });
-    }
-    // Process inline component templates
-    document.querySelectorAll("template[_]").forEach(processComponentTemplate);
-    // Process component template imports
+        // Remove the template from the final markup
+        template.remove();
+    });
+    // Process and remove inline component templates
+    processTemplates(document);
+    // Process and remove component template imports
     document.querySelectorAll("iframe.template-import").forEach(templateImport => {
-        templateImport.onload = () => templateImport.contentDocument.querySelectorAll("template[_]").forEach(processComponentTemplate);
+        templateImport.onload = _ => {
+            processTemplates(templateImport.contentDocument);
+            templateImport.remove();
+        }
     });
 })();
